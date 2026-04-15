71f6e331a8a26f3d6b4d5459c183ab3903bcecd3
Rasel-Khalipha <80430597+Rasel-khalipha@users.noreply.github.com>
2026-04-15 20:24:40 +0600

    keen keeper timeline call, message, video functionality done

--- src/pages/Timeline/TimeLine.jsx
+++ src/pages/Timeline/TimeLine.jsx
@@ -3,6 +3,7 @@ import { FriendContext } from "../../context/FriendContext/FriendContext";
 import TimelineCall from "../../components/timelineDetails/TimelineCall/TimelineCall";
 import TimelineMessage from "../../components/timelineDetails/TimelineMessage/TimelineMessage";
 import TimelineVideo from "../../components/timelineDetails/TimelineVideo/TimelineVideo";
+import { FaAngleDown } from "react-icons/fa6";
 
 const TimeLine = () => {
 	const { voiceCall, message, videoCall } = useContext(FriendContext);
@@ -26,6 +27,29 @@ const TimeLine = () => {
 		<>
 			<div className="w-[73.4%] mx-auto py-20 space-y-6">
 				<h2 className="font-bold text-5xl text-[#1F2937] mb-6">Timeline</h2>
+				<div className="dropdown dropdown-start">
+					<div
+						tabIndex={0}
+						role="button"
+						className="btn m-1 max-w-[347px] flex justify-between font-normal text-lg text-[#64748B]"
+					>
+						Filter timeline <FaAngleDown />
+					</div>
+					<ul
+						tabIndex="-1"
+						className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+					>
+						<li>
+							<a>Call</a>
+						</li>
+						<li>
+							<a>Text</a>
+						</li>
+						<li>
+							<a>Video</a>
+						</li>
+					</ul>
+				</div>
 				{timelineEvents.length > 0 ? (
 					timelineEvents.map((event) =>
 						event.type === "call" ? (
